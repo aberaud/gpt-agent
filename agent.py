7823421173bd3bcd0832612f47f90261d85b4471
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2023-06-05 21:37:00 -0400

    improvements

--- agent.py
+++ agent.py
@@ -1,64 +1,80 @@
 import asyncio
 import subprocess
 import datetime
+import traceback
+from typing import Optional
 import yaml
 from chat import ChatSession
 from web_server import WebServer
 from search import search
 
-system_prompt_syntax = f'date: "{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}"' + """
-instructions: |
-  Always communicate in valid YAML format, directly and without any other introduction, comment or text.
-  Always only output a single action to take (you will have the opportunity for more actions later), one of:
-  * Note information for future reference:
-      {"type": "info", "message": "this is some information"}
-    or:
-      type: info
-      message: |
-        this is some
-        multiline information
-  * Lookup for information. Available sources: knowledge-graph, wikipedia, google
-      {"type": "query", "source":"knowledge-graph", "query": "Paris"}
-  * Write to a file:
-      type: write
-      file: file.txt
-      content: |
-        this is some 
-        multiline file content
-  * Ask for more information to the human supervisor:
-      {"type": "request", "message": "should we use rust or go?"}
-  * Assign a subtask to another agent. Provide a detailed description of the task and an id.
-      {"type": "assign", "id": "create", "task": "create the initial project structure"}
-  * Run one or more bash commands and get the output:
-      {"type": "command", "command": "ls -la"}
-     or: 
-      type: command
-      command:
-        - mkdir project
-        - cd project
-        - git init
-  * Notify task completion. The parent agent will be provided a corresponding 'completed' message.
-      {"type": "complete", "message": "empty repo created"}
+purpose_agent = """You are an autonomous agent whose purpose is to solve a long-term task provided by a human supervisor.
+Your goal is to analyse the problem and break it down into smaller subtasks that can be solved by other agents, until the task is complete.
+Always double check your work and the work of other agents."""
+
+purpose_subagent = """You are an autonomous agent whose purpose is to solve a long-term task provided by a human supervisor.
+Your goal is to analyse the problem and break it down into smaller subtasks that can be solved by other agents, until the task is complete.
+Always double check your work and the work of other agents.
+After you complete, your history will be lost and only our completion message will be preserved, along with any change made to the system.
 """
 
-system_prompt = """
-purpose: You are an autonomous agent whose purpose is to solve a long-term task provided by a human supervisor.
-  Your goal is to analyse the problem and break it down into smaller subtasks that can be solved by other agents,
-  until the task is complete.
-""" + system_prompt_syntax
+instructions = """Always communicate in valid YAML format, directly and without any other introduction, comment or text.
+Always only output a single action to take (you will have the opportunity for more actions later), one of:
+* Note information for future reference:
+    {"type": "info", "message": "this is some information"}
+  or:
+    type: info
+    message: |
+        this is some
+        multiline information
+* Lookup for information. Available sources: knowledge-graph, wikipedia, google
+    {"type": "query", "source":"knowledge-graph", "query": "Paris"}
+* Write to a file (overrides existing content, if any):
+    type: write
+    file: file.txt
+    content: |
+        this is some
+        multiline file content
+* Ask for more information to a supervisor (human or supervisor agent) - don't assign tasks or report status that way:
+    {"type": "request", "to": "human", "message": "should we use rust or go?"}
+* Assign a subtask to another agent. Provide a detailed description of the task and an id.
+    {"type": "assign", "id": "create", "task": "create the initial project structure"}
+* Run one or more shell command and get the output. Note that the shell is reset between each command.
+    {"type": "command", "command": "ls -la"}
+  or
+    {"type": "command", "command": "cat file.txt"}
+  or:
+    type: command
+    command:
+        - mkdir project
+        - cd project && git init
+* Notify task completion (either success or failure). The parent agent will be provided a corresponding 'completed' message.
+  If you are stuck in a failure loop, complete and notify the parent agent.
+    {"type": "complete", "status": "success", "message": "empty repo created"}
+"""
 
-system_prompt_subagent = """
-purpose: You are an autonomous agent whose purpose is to help solving a long-term goal provided by a human supervisor.
-  Your objective is to complete a task provided to you by another agent.
-  Analyse the long-term goal and the task that you have been assigned,
-  complete the task yourself or break it down into smaller tasks that can be solved by other agents.
-""" + system_prompt_syntax
+
+def getPurpose(type='agent'):
+    if type == 'agent':
+        return purpose_agent
+    elif type == 'subagent':
+        return purpose_subagent
+
+def getSystemPrompt(type='agent'):
+    return yaml.safe_dump({
+        "type": "task",
+        "date": datetime.datetime.now(),
+        "instructions": instructions,
+        "task": getPurpose(type)
+    })
 
 class Agent:
-    def __init__(self, chat_session: ChatSession, web_server: WebServer, name: str = "main"):
+    def __init__(self, chat_session: ChatSession, web_server: WebServer, name: str = "main", parent: Optional['Agent']=None):
         self.chat_session = chat_session
         self.name = name
         self.web_server = web_server
+        self.parent = parent
+        self.supervisor_path = ['human'] if parent is None else parent.supervisor_path + [parent.name]
         print(f"Agent {name} ({chat_session.model}) created")
 
     async def send_update(self):
@@ -67,6 +83,7 @@ class Agent:
                 'state': 'running',
                 'agents': [{
                     'id': self.name,
+                    'path': self.supervisor_path,
                     'messages': self.chat_session.messages
                 }]
             })
@@ -86,10 +103,12 @@ class Agent:
                     print(f"[PARSE ERROR] Couldn't parse agent's message: {response}", e)
                     self.chat_session.add_message(yaml.dump({
                         "type": "parse_error",
-                        "message": str(e)
+                        "message": f"YAMLError: {e}",
+                        "hint": "Make sure you are using valid YAML format. Careful with multiline strings."
                     }), "system")
                 except Exception as e:
-                    print(f"[ERROR] Couldn't handle agent's message: {response}", e)
+                    print(f"[ERROR] Couldn't handle agent's message: {response}")
+                    print(traceback.format_exc())
                     self.chat_session.add_message(yaml.dump({
                         "type": "error",
                         "message": str(e)
@@ -105,7 +124,9 @@ class Agent:
         if type == "info":
             print(f"[INFO] {command['message']}")
         elif type == "request":
-            print(f"[REQUEST] {command['message']}")
+            request = command['message']
+            to = command['to']
+            print(f"[REQUEST] for {to}: {request}")
             user_input = await self.web_server.get_input({
                 'state': 'request',
                 'id': self.name,
@@ -116,34 +137,7 @@ class Agent:
                 "message": user_input
             }))
         elif type == "assign":
-            sub_agent_id=command['id']
-            print(f"[ASSIGN] {sub_agent_id} {command['task']}")
-            await self.send_update()
-            sub_chat_session = ChatSession(self.chat_session.model, system_prompt_subagent)
-            sub_chat_session.messages += self.chat_session.messages[1:-1]
-            sub_chat_session.add_message(yaml.dump({
-                "instructions": f"You are now {sub_agent_id}. Complete your task.",
-                #"date": datetime.datetime.now(),
-                'task': command['task']
-            }), "system")
-            sub_agent = Agent(sub_chat_session, web_server=self.web_server, name=sub_agent_id)
-            await sub_agent.run()
-            last_msg = sub_chat_session.messages[-1].content
-            try:
-                last_msg_parsed = yaml.safe_load(last_msg)
-                print(f"[ASSIGN] {command['id']} {command['task']} completed: {last_msg_parsed}")
-                self.chat_session.add_message(yaml.dump({
-                    "type": "completed",
-                    "id": command['id'],
-                    "message": last_msg_parsed['message']
-                }))
-            except yaml.YAMLError as e:
-                print(e)
-                self.chat_session.add_message(yaml.dump({
-                    "type": "completed",
-                    "id": command['id'],
-                    "message": last_msg
-                }))
+            await self.handle_agent_assign(command)
 
         elif type == "query":
             source = command['source']
@@ -193,6 +187,62 @@ class Agent:
             print(f"[ERROR] Unknown command type: {command['type']}")
         return False
 
+    @staticmethod
+    def parse_message(message):
+        try:
+            return yaml.safe_load(message)
+        except yaml.YAMLError as e:
+            #print(f"[PARSE ERROR] Couldn't parse agent's message: {message}", e)
+            return None
+
+    def convert_message_for_subagent(self, message):
+        #print('convert_message_for_subagent', message)
+        msg = Agent.parse_message(message['content'])
+        if msg is None:
+            return message
+        if message['role'] == 'system' and msg['type'] == 'task':
+            return {
+                'role': 'system',
+                'content': yaml.dump({
+                    'type': 'assign',
+                    'task': msg['task'],
+                    'id': self.name
+                })
+            }
+        return message
+
+    def convert_history_for_subagent(self):
+        agent_history = self.chat_session.messages[1:-1]
+        agent_history = [self.convert_message_for_subagent(h) for h in agent_history]
+        return [h for h in agent_history if h is not None]
+
+    async def handle_agent_assign(self, command):
+        sub_agent_id=command['id']
+        task=command['task']
+        print(f"[ASSIGN] {sub_agent_id} {task}")
+        await self.send_update()
+        sub_chat_session = ChatSession(self.chat_session.model, getSystemPrompt('subagent'))
+        sub_chat_session.messages += self.convert_history_for_subagent()#self.chat_session.messages[1:-1]
+        sub_chat_session.add_message(yaml.dump({
+            "type": "task",
+            "instructions": f"You are now the Agent '{sub_agent_id}'. Complete your task.",
+            "supervisor_path": self.supervisor_path + [self.name],
+            "date": datetime.datetime.now(),
+            'task': task
+        }), "system")
+        sub_agent = Agent(sub_chat_session, web_server=self.web_server, name=sub_agent_id, parent=self)
+        await sub_agent.run()
+        last_msg = sub_chat_session.messages[-1].content
+        last_msg_parsed = Agent.parse_message(last_msg)
+        print(f"[ASSIGN] {sub_agent_id} {task} completed: {last_msg_parsed}")
+        self.chat_session.add_message(yaml.dump({
+            "type": "completed",
+            "id": sub_agent_id,
+            "message": last_msg_parsed['message'] if last_msg_parsed is not None else last_msg,
+            "status": last_msg_parsed['status'] if last_msg_parsed is not None else 'success'
+        }))
+
+
     def handle_agent_process(self, command):
         try:
             subprocess_result = subprocess.run(command, shell=True, capture_output=True, text=True)
@@ -231,7 +281,7 @@ async def execute_chat(chat_session, web_server):
 
 
 async def main(args):
-    chat_session = ChatSession(args.model, system_prompt)
+    chat_session = ChatSession(args.model, getSystemPrompt())
     web_server = WebServer()
     await asyncio.gather(
         web_server.run(),
