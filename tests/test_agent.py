6bab6a782cb2456819243b833365a07ab04df7fb
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2023-07-05 19:43:45 -0400

    improve tests

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import os
 import unittest
@@ -7,10 +8,7 @@ from app.agent_runner import AgentRunner
 
 class TestAgent(aiounittest.AsyncTestCase):
 
-    async def test_hello_world(self):
-        args = {'model': 'gpt-3.5-turbo-16k-0613'}
-        context = AgentRunner(args)
-        await context.run("complete with message 'hello world'")
+    def check_complete(self, context: AgentRunner, expected: str | None = None):
         last_message = context.main_agent.chat_session.last_message()
         self.assertIsNotNone(last_message)
         print(last_message)
@@ -19,24 +17,42 @@ class TestAgent(aiounittest.AsyncTestCase):
         arguments = function_call.get('arguments')
         self.assertIsNotNone(arguments)
         parsed = json.loads(arguments)
-        self.assertEqual(parsed['content'], 'hello world')
-    
+        if expected is not None:
+            self.assertEqual(parsed['content'], expected)
 
+    async def test_hello_world(self):
+        args = {'model': 'gpt-3.5-turbo-16k-0613'}
+        context = AgentRunner(args)
+        await context.run("complete with message 'hello world'")
+        self.check_complete(context, 'hello world')
+    
     async def test_create_directory(self):
         args = {'model': 'gpt-3.5-turbo-16k-0613'}
         context = AgentRunner(args)
         await context.run("create empty directory 'test'")
-        last_message = context.main_agent.chat_session.last_message()
-        self.assertIsNotNone(last_message)
-        print(last_message)
-        function_call = last_message.get('function_call')
-        self.assertIsNotNone(function_call)
+        self.check_complete(context)
         test_path = os.path.join(context.path, 'test')
         self.assertTrue(os.path.exists(test_path))
         self.assertTrue(os.path.isdir(test_path))
         os.rmdir(test_path)
         os.rmdir(context.path)
 
+    async def test_simple_script(self):
+        args = {'model': 'gpt-3.5-turbo-16k-0613'}
+        context = AgentRunner(args)
+        await context.run("create a simple python script 'test.py' that just prints 'hello world'")
+        self.check_complete(context)
+        test_script = os.path.join(context.path, 'test.py')
+        self.assertTrue(os.path.exists(test_script))
+        self.assertTrue(os.path.isfile(test_script))
+        process = await asyncio.create_subprocess_exec('python3', test_script, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await process.communicate()    
+        return_code = process.returncode
+        process = None
+        self.assertEqual(return_code, 0)
+        self.assertEqual(stdout.decode().strip().lower(), 'hello world')
+        os.remove(test_script)
+        os.rmdir(context.path)
 
 if __name__ == '__main__':
     unittest.main()
